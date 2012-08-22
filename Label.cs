1a25d39eec1d15c2d5422139eb1303c0fc3de0d9
evandrojr <evandrojr@gmail.com>
2012-08-22 14:22:54 -0300

    1st push

---
+++ Label.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniClient
+{
+    public class Label
+    {
+        public string Id;
+        public List<string> MemberLst = new List<string>();
+        public static List<Label> LabelLst = new List<Label>();
+
+        public Label() {
+        }
+
+        public void Save(){
+
+        }
+        
+    }
+}
